1b956f8bd59e908550a54cd81436cf74b20255b0
Vishal Madhvani <vishal.madhvani@microsoft.com>
2020-08-26 10:57:26 +0100

    add nunit-sample project

---
+++ nunit-sample/FixtureB.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace nunit_sample
+{
+    [TestFixture]
+    public class FixtureB
+    {
+        [Test]
+        public void PassingTest()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void FailingTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
